697085d5063fd48d2a6c3bd878d3ef65f4d02698
yababavideo <66917446+yababavideo@users.noreply.github.com>
2022-11-06 17:25:43 -0500

    Delete testgameone.js

--- testgameone.js
+++
@@ -1,7 +0,0 @@
-let iceCream = "chocolagte";
-
-if (iceCream === "chocolate") {
-    alert("Yay I love chocolate ice cream");
-} else {
-    alert("bruh, but chocolate icea cream is my favorite");
-}
