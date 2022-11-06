edad9531d39865ab84c2bb58fa246998123de31c
yababavideo <66917446+yababavideo@users.noreply.github.com>
2022-11-06 17:41:13 -0500

    Create test.js
    testing operators in javascript

---
+++ test.js
@@ -0,0 +1,7 @@
+let iceCream = "chocolagte";
+
+if (iceCream === "chocolate") {
+    alert("Yay I love chocolate ice cream");
+} else {
+    alert("bruh, but chocolate icea cream is my favorite");
+}
